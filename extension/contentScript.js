11b8b8f143f9fbbb51b23e8190519347c8e13e00
magnuswatn <magnus@watn.no>
2017-04-01 14:35:29 +0200

    Javascript style cleanup

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,5 +1,5 @@
 // Legger inn en fin liten hyggelig beskjed i Commfides-boksen
-document.getElementById('identification').innerHTML = '<p>Logg inn ved å trykke på C-knappen i adresselinjen</p>'
+document.getElementById('identification').innerHTML = '<p>Logg inn ved å trykke på C-knappen i adresselinjen</p>';
 
 // Injiserer et script inn i siden, for å hente ut parameterene (hash, controlValue++) fra Commfides
 // https://stackoverflow.com/questions/9602022/chrome-extension-retrieving-gmails-original-message
@@ -12,47 +12,48 @@ getParamsScript.onload = function() {
 
 // Henter ut responsen fra scriptet som ble injisert i siden
 document.addEventListener('RW759_connectExtension', function(e) {
-	parameters = e.detail;
+    parameters = e.detail;
 });
 
 function sendParametersToCommfides(certificate, hash, controlValue) {
-	// Blåkopi av scriptet som kommer fra Commfides
-	// Populerer et skjult skjema, og sender det inn
+    // Blåkopi av scriptet som kommer fra Commfides
+    // Populerer et skjult skjema, og sender det inn
 
-	var form = document.getElementById("frmResult");
-	
-	var hiddenUserCertificate = document.createElement("input");
-	hiddenUserCertificate.setAttribute("type", "hidden");
-	hiddenUserCertificate.setAttribute("name", "userCertificate");
-	hiddenUserCertificate.setAttribute("value", certificate);
-	
-	var hiddenSignedHash = document.createElement("input");
-	hiddenSignedHash.setAttribute("type", "hidden");
-	hiddenSignedHash.setAttribute("name", "signedHash");
-	hiddenSignedHash.setAttribute("value", hash);
-	
-	var hiddenControlValueOut = document.createElement("input");
-	hiddenControlValueOut.setAttribute("type", "hidden");
-	hiddenControlValueOut.setAttribute("name", "controlValueOut");
-	hiddenControlValueOut.setAttribute("value", controlValue);
-	
-	form.appendChild(hiddenUserCertificate);
-	form.appendChild(hiddenSignedHash);
-	form.appendChild(hiddenControlValueOut);
+    var form = document.getElementById("frmResult");
 
-	document.body.appendChild(form);
+    var hiddenUserCertificate = document.createElement("input");
+    hiddenUserCertificate.setAttribute("type", "hidden");
+    hiddenUserCertificate.setAttribute("name", "userCertificate");
+    hiddenUserCertificate.setAttribute("value", certificate);
 
-	form.submit();
+    var hiddenSignedHash = document.createElement("input");
+    hiddenSignedHash.setAttribute("type", "hidden");
+    hiddenSignedHash.setAttribute("name", "signedHash");
+    hiddenSignedHash.setAttribute("value", hash);
+
+    var hiddenControlValueOut = document.createElement("input");
+    hiddenControlValueOut.setAttribute("type", "hidden");
+    hiddenControlValueOut.setAttribute("name", "controlValueOut");
+    hiddenControlValueOut.setAttribute("value", controlValue);
+
+    form.appendChild(hiddenUserCertificate);
+    form.appendChild(hiddenSignedHash);
+    form.appendChild(hiddenControlValueOut);
+
+    document.body.appendChild(form);
+
+    form.submit();
 }
 
 // Lytter etter forespørsler fra popup-en, og svarer med parameterne fra Commfides
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-       if (request.operation == "gimmeGimmeGimmeSomeParametersAfterMidnight") {
-		sendResponse({parameters: parameters});
-	} else if (request.operation == "signInStranger") {
-		sendParametersToCommfides(request.certificate, request.hash, request.controlValue)
-	} else {
-		console.log('what')
-	}
-  });
+    function(request, sender, sendResponse) {
+        if (request.operation == "gimmeGimmeGimmeSomeParametersAfterMidnight") {
+            sendResponse({parameters: parameters});
+        } else if (request.operation == "signInStranger") {
+            sendParametersToCommfides(request.certificate, request.hash, request.controlValue)
+        } else {
+            console.log('what')
+        }
+    }
+);
--- extension/popup.js
+++ extension/popup.js
@@ -1,41 +1,41 @@
 // Kaller content-scriptet for å hente ut parameterne fra Commfides
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {operation: "gimmeGimmeGimmeSomeParametersAfterMidnight"}, function(response) {
-	// Sjekker om vi fikk noe svar, vis isåfall innloggingselementene på siden
-	if (response != null){
-		parameters = response.parameters;
-		document.getElementById("text").innerHTML="Skriv inn pin-kode for &aring logge inn:";
-		document.getElementById("form").style.display = "";
-}
+    chrome.tabs.sendMessage(tabs[0].id, {operation: "gimmeGimmeGimmeSomeParametersAfterMidnight"}, function(response) {
+    // Sjekker om vi fikk noe svar, vis isåfall innloggingselementene på siden
+        if (response != null){
+            parameters = response.parameters;
+            document.getElementById("text").innerHTML="Skriv inn pin-kode for &aring logge inn:";
+            document.getElementById("form").style.display = "";
+        }
   });
 });
 
 function Sign(pin) {
-	// Kaller Python-appen for å signere parameterne
-	var msg = { "operation": "sign", "params": {"pin": pin, "hash": parameters.hash} }
-	chrome.runtime.sendNativeMessage('no.watn.magnus.smartcardsignapp', msg, function(response){
-	if (response.status == "success"){
-		SendParametersToCommfides(response.params.cert, response.params.signedHash, parameters.controlValue);
-	} else {
-		alert("Det skjedde en feil: " + response.message);
-		window.close()
-	}
-	})
+    // Kaller Python-appen for å signere parameterne
+    var msg = { "operation": "sign", "params": {"pin": pin, "hash": parameters.hash} };
+    chrome.runtime.sendNativeMessage('no.watn.magnus.smartcardsignapp', msg, function(response){
+        if (response.status == "success"){
+            SendParametersToCommfides(response.params.cert, response.params.signedHash, parameters.controlValue);
+        } else {
+            alert("Det skjedde en feil: " + response.message);
+            window.close()
+        }
+    });
 }
 
 function SendParametersToCommfides(certificate, hash, controlValue) {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {operation: "signInStranger", certificate: certificate, hash: hash, controlValue: controlValue}, function(response) {
-		window.close();
-	  });
-	});
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {operation: "signInStranger", certificate: certificate, hash: hash, controlValue: controlValue}, function(response) {
+            window.close();
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('form').addEventListener('submit', function(evt) {
-		evt.preventDefault()
-		document.getElementById('text').textContent='Logger inn...';
-	  	document.getElementById('form').style.display="none";
-		Sign(document.getElementById('pin').value)
-  }, false);
+    document.getElementById('form').addEventListener('submit', function(evt) {
+        evt.preventDefault()
+        document.getElementById('text').textContent='Logger inn...';
+        document.getElementById('form').style.display="none";
+        Sign(document.getElementById('pin').value);
+    }, false);
 }, false);
